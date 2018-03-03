24a62d4ac19856fea26e6a8b009662d8c8b5ae6c
Emerentius <emerentius@arcor.de>
2018-03-03 09:50:43 +0100

    add project euler example

---
+++ examples/project_euler.rs
@@ -0,0 +1,22 @@
+// Solution to Problem 096 of Project Euler
+// https://projecteuler.net/problem=96
+//
+// For demonstration purposes only. If you're gonna use this
+// you may as well not do the problem at all
+
+extern crate sudoku;
+use sudoku::Sudoku;
+
+fn main() {
+    let mut sum = 0;
+    let sudokus = include_str!("p096_sudoku.txt");
+    for sudoku in sudokus.split("Grid")
+        .skip(1)
+        .map(Sudoku::from_str_block_permissive)
+        .map(Result::unwrap)
+    {
+        let array = sudoku.solve_one().unwrap().to_bytes();
+        sum += array[..3].iter().fold(0, |acc, &num| acc*10 + num as u32);
+    }
+    println!("{}", sum);
+}

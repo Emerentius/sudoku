6f4064738c7063ba70fed6ab7fd91a90996f3aef
Emerentius <emerentius@arcor.de>
2019-03-21 22:49:19 +0100

    add more trait implementations to CellState
    not sure yet whether defining an order is desirable so *Ord
    are left out

--- src/board/cell_state.rs
+++ src/board/cell_state.rs
@@ -2,7 +2,7 @@ use crate::board::Digit;
 use crate::bitset::Set;
 
 /// Contains either a digit or all the candidates for an unsolved cell
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, PartialEq, Eq, Debug, Hash)]
 #[allow(missing_docs)]
 pub enum CellState {
     Digit(Digit),
